019a61b498b77a8df00c9eb55641a28c1a510232
Amirabas Ahmadnia <ahmadniaamirabas@gmail.com>
2024-05-10 18:16:29 +0330

    feat: Add deleteUser functionality to users controller

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getUsers } from './../db/users';
+import { deleteUserById, getUsers } from './../db/users';
 
 export const getAllUsers = async (
   req: express.Request,
@@ -8,8 +8,23 @@ export const getAllUsers = async (
 ) => {
   try {
     const users = await getUsers();
-
     return res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.status(400);
+  }
+};
+
+export const deleteUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+
+    const deletedUser = await deleteUserById(id);
+
+    return res.json(deletedUser);
   } catch (error) {
     console.log(error);
     return res.status(400);
